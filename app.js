52b05066fe6ed354daac071c9cf66968418af91d
Michael Bosworth <mtbosworth@gmail.com>
2014-12-23 18:19:23 -0600

    excpetion handling

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // Start sails and pass it command line arguments
 require('sails').lift(require('optimist').argv);
+
+process.on('uncaughtException', function(err) {
+    console.log("Uncaught exception!", err);
+});

97a0bdde7ecd87d4ff2a9be8b51f311ac7e4d39e
Michael Bosworth <michael.bosworth@bunchball.com>
2015-10-15 08:27:22 -0500

    fix app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Start sails and pass it command line arguments
 require('sails').lift(require('optimist').argv);
 
+// handle errors
 process.on('uncaughtException', function(err) {
     console.log("Uncaught exception!", err);
 });

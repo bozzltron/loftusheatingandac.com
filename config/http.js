45dc4a03a066b23c1f20c98c2aa58f0d60341a54
Michael <michael.bosworth@bunchball.com>
2021-04-19 16:08:09 -0500

    add flash middleware

--- config/http.js
+++ config/http.js
@@ -22,7 +22,7 @@ module.exports.http = {
   ****************************************************************************/
 
   middleware: {
-
+    flash    : require('connect-flash')(),
   /***************************************************************************
   *                                                                          *
   * The order in which middleware should be run for HTTP request. (the Sails *
